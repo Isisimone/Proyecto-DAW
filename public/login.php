aea62e8a439a3b5b33ddce07dc924e5a0468a5e8
Isis <isis_simone@hotmail.com>
2025-04-05 20:36:48 +0200

    Added login page styles and updated login PHP

--- public/login.php
+++ public/login.php
@@ -48,12 +48,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <style>
-        .error {
-            color: red;
-            margin: 10px 0;
-        }
-    </style>
+  <link rel="stylesheet" href="../css/login.css">
+
 </head>
 <body>
     <h1>Iniciar Sesión</h1>
